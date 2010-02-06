54ff8c4e9152f029e9c4dcac524af3af346fb178
Souheil CHELFOUH <trollfot@gmail.com>
2010-02-06 12:50:17 +0000

    Added ZPL headers.

--- src/grokcore/content/interfaces.py
+++ src/grokcore/content/interfaces.py
@@ -1,5 +1,18 @@
-# -*- coding: utf-8 -*-
-
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Specific content interfaces for Grok.
+"""
 from grokcore.component.interfaces import IContext
 from zope.container.interfaces import IOrderedContainer
 from zope.container.interfaces import IContainer as IContainerBase
